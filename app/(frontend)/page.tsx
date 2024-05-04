1ffee95502f3c1cc85f2e53962d513443f4ab7a4
TahaNamdar <tahanamdar30@gmail.com>
2024-05-04 12:28:33 +0330

    develop: render latest and featured product {SSR}

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,13 +1,60 @@
 import ProductItem from "@/components/product/productItem";
 import React from "react";
-import { data } from "@/lib/data";
-export default function Home() {
+import { Metadata } from "next";
+import productService from "@/lib/services/productService";
+
+const metadata: Metadata = {
+  title: process.env.NEXT_PUBLIC_APP_NAME,
+  description: process.env.DESC,
+};
+
+export default async function Home() {
+  const featuredProducts = await productService.getFeatured();
+  const latestProducts = await productService.getLatest();
+
   return (
     <div>
       <h2 className="p-4">Latest Product</h2>
       <div>
+        {/* featured Products */}
+        <div className="carousel w-full mb-20">
+          {featuredProducts.map((product, index) => {
+            return (
+              <div
+                id={`slide-${index}`}
+                className="carousel-item relative w-full"
+                key={index}
+              >
+                <img
+                  src={product.banner}
+                  className="h-96 w-full object-cover "
+                  alt={product.name}
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a
+                    href={`#slide-${
+                      index == 0 ? featuredProducts.length - 1 : index - 1
+                    }`}
+                    className="btn btn-circle"
+                  >
+                    ❮
+                  </a>
+                  <a
+                    href={`#slide-${
+                      index == featuredProducts.length - 1 ? 0 : index + 1
+                    }`}
+                    className="btn btn-circle"
+                  >
+                    ❯
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {/* latest product */}
         <div className="grid mx-auto gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          {data.products.map((product, _) => {
+          {latestProducts.map((product, _) => {
             return <ProductItem key={product.slug} product={product} />;
           })}
         </div>
--- lib/services/productService.ts
+++ lib/services/productService.ts
@@ -1,3 +1,33 @@
 import { cache } from "react";
+import dbConnect from "../dbConnect";
+import ProductModel, { Product } from "../models/productModel";
 
+//every hour
 export const revalidate = 3600;
+
+const getLatest = cache(async () => {
+  await dbConnect();
+  const products = await ProductModel.find({})
+    .sort({ _id: -1 })
+    .limit(4)
+    .lean();
+  return products as Product[];
+});
+
+const getFeatured = cache(async () => {
+  await dbConnect();
+  const products = await ProductModel.find({ isFeatured: true })
+    .limit(3)
+    .lean();
+  return products;
+});
+
+const getBySlug = cache(async (slug: string) => {
+  await dbConnect();
+  const product = ProductModel.findOne({ slug }).lean();
+  return product;
+});
+
+const productService = { getLatest, getFeatured, getBySlug };
+
+export default productService;
